39f9c4db4bbee7b70f381d123fe68c4ed9467dbd
vbelfoure@axway.com <vbelfoure@axway.com>
2023-02-28 09:57:20 -0700

    Modified 17 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjE2NzExOQ/spectral.js?branch_slug=master&token=de6ceb95-f56e-4c14-9ab8-8622dfc80913";
+export default { extends: ruleset };
